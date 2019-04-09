3deec63b34b2864d6852a33f17d23b4883591fdd
Raziel Willms <42423628+RazielWillms@users.noreply.github.com>
2019-04-09 20:35:51 -0300

    Inicio da construção do menu

---
+++ menu.php
@@ -0,0 +1,53 @@
+<html>
+<title>
+    Menu
+</title>
+<body>
+<ul>
+    <li><a href="cadastroCliente.php">Home</a></li>
+    <li><a href="cadastroCliente.php">Cadastro de Cliente</a></li>
+    <li><a href="cadastroCliente.php">Comprar Ingressos</a></li>
+</ul>
+</body>
+</html>
+<style>
+    body{
+        padding-top: 20px;
+        padding-left: 40px;
+    }
+
+    ul li{
+        list-style-type: none;
+        display: inline;
+        padding-left: 15px;
+        padding-right: 15px;
+        background-color: red;
+        font-family: "Trebuchet MS", Arial, "Microsoft YaHei Light", SansSerif;
+        font-size: 15px;
+        color: black;
+        border-radius: 3px;
+        -moz-border-radius: 3px;
+        -webkit-border-radius: 3px;
+    }
+
+    ul li a{
+        font-family: "Trebuchet MS", Arial, "Microsoft YaHei Light", SansSerif;
+        font-size: 15px;
+        color: black;
+        text-decoration: none;
+    }
+
+    ul li: animateTransform{
+        list-style-type: none;
+        display: inline;
+        padding-left: 15px;
+        padding-right: 15px;
+        background-color: black;
+        font-family: "Trebuchet MS", Arial, "Microsoft YaHei Light", SansSerif;
+        font-size: 25px;
+        color: red;
+        border-radius: 3px;
+        -moz-border-radius: 3px;
+        -webkit-border-radius: 3px;
+    }
+</style>
