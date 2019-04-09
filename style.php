dc8233b4323366f8471f20ddb3473afb2c866e36
Raziel Willms <42423628+RazielWillms@users.noreply.github.com>
2019-04-09 20:36:38 -0300

    modelagem css para as páginas

---
+++ style.php
@@ -0,0 +1,36 @@
+<style type=text/css>
+    body {
+        background-image: url("img/fundo4.jpg");
+    }
+
+    estiloTitle {
+        font: bold 20px Broadway, Arial, Sans-Serif;
+
+        font-size: 35px;
+        color: silver;
+        font-weight: bolder;
+    }
+
+    estiloform {
+        font: bold 20px Helvetica, Arial, Sans-Serif;
+        font-size: 20px;
+        color: silver;
+        font-weight: bolder;
+        text-shadow: 0px 2px 2px rgba(255, 255, 255, 0.4);
+    }
+    estilobody{
+        font: bold 20px Helvetica, Arial, Sans-Serif;
+        color: mintcream;
+        font-weight: bolder;
+        text-shadow: 1px 1px 1px black, 3px 3px 5px black;
+    }
+
+    a {
+        background-color: red;
+        color: white;
+        font-size: 15px;
+        text-decoration: none;
+        text-transform: uppercase;
+        text-shadow: none;
+    }
+</style>
