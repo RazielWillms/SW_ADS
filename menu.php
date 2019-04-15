1dc472821d8aebfa027ba2df6e33992c024e1da7
Raziel Willms <42423628+RazielWillms@users.noreply.github.com>
2019-04-15 17:26:21 -0300

    inserção de dados fictícios no menu

--- menu.php
+++ menu.php
@@ -2,52 +2,31 @@
 <title>
     Menu
 </title>
+<style type="text/css">
+    img{width: 350px;height: 250px;}
+</style>
 <body>
 <ul>
-    <li><a href="cadastroCliente.php">Home</a></li>
+    <li><a href="menu.php">Home</a></li>
     <li><a href="cadastroCliente.php">Cadastro de Cliente</a></li>
-    <li><a href="cadastroCliente.php">Comprar Ingressos</a></li>
+    <li><a href="tipoIngresso.php">Comprar Ingressos</a></li>
 </ul>
+<imagem>
+    <img src="img/sepultura.jpeg" ;><br>
+</imagem>
+
+<estilomenu>
+    SEPULTURA 23/11<br>
+</estilomenu>
+<estilobody>
+    Estado: Rio Grande do Sul<br>
+    Cidade: Santo Ângelo<br>
+    Horário: 23:30<br>
+</estilobody>
+
 </body>
 </html>
-<style>
-    body{
-        padding-top: 20px;
-        padding-left: 40px;
-    }
 
-    ul li{
-        list-style-type: none;
-        display: inline;
-        padding-left: 15px;
-        padding-right: 15px;
-        background-color: red;
-        font-family: "Trebuchet MS", Arial, "Microsoft YaHei Light", SansSerif;
-        font-size: 15px;
-        color: black;
-        border-radius: 3px;
-        -moz-border-radius: 3px;
-        -webkit-border-radius: 3px;
-    }
-
-    ul li a{
-        font-family: "Trebuchet MS", Arial, "Microsoft YaHei Light", SansSerif;
-        font-size: 15px;
-        color: black;
-        text-decoration: none;
-    }
-
-    ul li: animateTransform{
-        list-style-type: none;
-        display: inline;
-        padding-left: 15px;
-        padding-right: 15px;
-        background-color: black;
-        font-family: "Trebuchet MS", Arial, "Microsoft YaHei Light", SansSerif;
-        font-size: 25px;
-        color: red;
-        border-radius: 3px;
-        -moz-border-radius: 3px;
-        -webkit-border-radius: 3px;
-    }
-</style>
+<?php
+require_once "style.php";
+?>
