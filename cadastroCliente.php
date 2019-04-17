900f971fe3ba56423acfc8a90a94ca8ba8e35bb7
Raziel Willms <42423628+RazielWillms@users.noreply.github.com>
2019-04-17 18:17:32 -0300

    Update

--- cadastroCliente.php
+++ cadastroCliente.php
@@ -1,34 +1,19 @@
-<estiloTitle>
-    <?php
-    require_once "style.php";
-    session_start();
-    if (session_id() == null || !isset($_SESSION['nome_cliente'])) {
-        ?>
-        <body>
-        <ul>
-            <li><a href="menu.php">Home</a></li>
-        </ul>
-        </body>
-        <?php
-        die('Usuário não logado!');
-
-    }
-    ?>
-</estiloTitle>
 <html>
 <title>
-    COMPRA INGRESSO
+    CADASTRO CLIENTE
 </title>
 <body>
 <ul>
     <li><a href="menu.php">Home</a></li>
+    <li><a href="endereco.php">Complementar cadastro</a> </li>
 </ul>
 </body>
 <head>
     <estiloTitle>
-        Compra do Ingresso<br><br>
+        Cadastro do Cliente<br><br>
     </estiloTitle>
 </head>
+
 </html>
 
 <?php
@@ -43,98 +28,85 @@ $deletarID = filter_input(INPUT_GET, 'deletarID', FILTER_VALIDATE_INT);
 
 // exibe cadastro e listagem
 if (empty($atualizarID) && empty($deletarID) && empty($action)) {
-    formingresso($pdo);
-    listarIngresso($pdo);
+    form();
+    listarCliente($pdo);
 }
 
 //cadastro
 if (empty($atualizarID) && empty($deletarID) && $action == 'Cadastrar') {
 
-    $idcliente = filter_input(INPUT_POST, 'id_cliente', FILTER_VALIDATE_INT);
-    $idingresso = filter_input(INPUT_POST, 'id_tipo_ingresso', FILTER_VALIDATE_INT);
-    $volumeingresso = filter_input(INPUT_POST, 'volume_ingresso', FILTER_VALIDATE_INT);
+    $nomeCliente = filter_input(INPUT_POST, 'nomeCliente', FILTER_SANITIZE_STRING);
+    $cpf = filter_input(INPUT_POST, 'cpf', FILTER_VALIDATE_INT);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+    $telefone = filter_input(INPUT_POST, 'telefone', FILTER_VALIDATE_INT);
+    $senhaAberta = filter_input(INPUT_POST, 'senha', FILTER_DEFAULT);
+    $senhaParaArmazenarNoBanco = password_hash($senhaAberta, PASSWORD_DEFAULT);
 
     //inserção BD
-    $comandoSQL = "INSERT INTO Tipo_Ingresso_Cliente(id_tipo_ingresso, id_cliente, volume_ingresso)
-                    VALUES('$idingresso', '$idcliente', ' $volumeingresso');";
+    $comandoSQL = "INSERT INTO Cliente(nome_cliente, cpf, email, telefone, senha)" . "VALUES('$nomeCliente', '$cpf', '$email', '$telefone','$senhaParaArmazenarNoBanco');";
 
     $pdo->exec($comandoSQL);
 
+    header("location: http://localhost/acessoBD/SW_AVALIA%C3%87%C3%83O/login.php");
 
-    formingresso($pdo);
-    listarIngresso($pdo);
+    form();
+    listarCliente($pdo);
 }
 
 //apagar
 if (empty($atualizarID) && !empty($deletarID) && empty($action)) {
     //delete BD
-    $comandoSQL = "delete from Tipo_Ingresso_Cliente where id_tipo_ingresso_cliente = $deletarID;";
+    $comandoSQL = "delete from Cliente where id_cliente = $deletarID;";
     $totalPagados = $pdo->exec($comandoSQL);
 
-    formingresso($pdo);
-    listarIngresso($pdo);
+    form();
+    listarCliente($pdo);
 }
 
 // exibe opção de atualizar no formulário
 if (!empty($atualizarID) && empty($deletarID) && empty($action)) {
 
-    $IngressoArray = consultarIngresso($pdo, $atualizarID);
+    $ClienteArray = consultarClientes($pdo, $atualizarID);
 
-    if (is_array($IngressoArray) && count($IngressoArray) > 0) {
-        $idcliente = $IngressoArray[0]['id_cliente'];
-        $idingresso = $IngressoArray[0]['id_tipo_ingresso'];
-        $volumeingresso = $IngressoArray[0]['volume_ingresso'];
+    if (is_array($ClienteArray) && count($ClienteArray) > 0) {
+        $id = $ClienteArray[0]['id_cliente'];
+        $nomeCliente = $ClienteArray[0]['nome_cliente'];
+        $cpf = $ClienteArray[0]['cpf'];
+        $email = $ClienteArray[0]['email'];
+        $telefone = $ClienteArray[0]['telefone'];
         ?>
-        <form method="post" action="tipoIngresso.php?atualizarID=<?php echo $atualizarID; ?>">
+        <form method="post" action="cadastroCliente.php?atualizarID=<?php echo $atualizarID; ?>">
             <estilobody>
-                Selecione o Cliente:
-                <select name="id_cliente">
-                    <?php
-                    $IngressoArray = consultarClientes($pdo);
-                    foreach ($IngressoArray as $ingressoDados) {
-
-                        $selected = ($ingressoDados['id_cliente'] == $idcliente) ? 'selected' : '';
-
-                        echo '<option ' . $selected . ' value="' . $ingressoDados['id_cliente'] . '">' . $ingressoDados['nome_cliente'] . '</option>' . PHP_EOL;
-                    }
-                    ?>
-                </select><br>
-                Selecione o Tipo de Ingresso:
-                <select name="id_tipo_ingresso">
-                    <?php
-                    $consulta = $pdo->query('SELECT * FROM Tipo_Ingresso');
-                    $IngressoArray = $consulta->fetchAll(PDO::FETCH_ASSOC);
-                    foreach ($IngressoArray as $ingressoDados) {
-
-                        $selected = ($ingressoDados['id_tipo_ingresso'] == $idingresso) ? 'selected' : '';
-
-                        echo '<option ' . $selected . ' value="' . $ingressoDados['id_tipo_ingresso'] . '">' . $ingressoDados['descricao_ingresso'] . '</option>' . PHP_EOL;
-                    }
-                    ?>
-                </select><br>
-                Quantidade: <input type="number" name="volume_ingresso" value="<?php echo $volumeingresso; ?>"/><br>
+                <input type="hidden" value="<?php echo $id; ?>" name="id">
+                Nome: <input type="text" value="<?php echo $nomeCliente; ?>" name="nomeCliente"/><br>
+                CPF : <input type="number" value="<?php echo $cpf; ?>" name="cpf"/><br>
+                Email: <input type="text" value="<?php echo $email; ?>" name="email"/><br>
+                Telefone: <input type="number" value="<?php echo $telefone; ?>" name="telefone"/><br>
 
                 <input type="submit" name="action" value="Atualizar"/>
-                <a href="tipoIngresso.php">Cancelar</a>
+                <a href="cadastroCliente.php">Cancelar</a>
+                <br>
             </estilobody>
         </form>
         <?php
     }
-    listarIngresso($pdo);
+    listarCliente($pdo);
+//    $atualizarID = null;
 }
 
 //atualizar
 if (!empty($atualizarID) && empty($deletarID) && $action == 'Atualizar') {
-    $idcliente = filter_input(INPUT_POST, 'id_cliente', FILTER_VALIDATE_INT);
-    $idingresso = filter_input(INPUT_POST, 'id_tipo_ingresso', FILTER_VALIDATE_INT);
-    $volumeingresso = filter_input(INPUT_POST, 'volume_ingresso', FILTER_VALIDATE_INT);
+    $nomeCliente = filter_input(INPUT_POST, 'nomeCliente', FILTER_SANITIZE_STRING);
+    $cpf = filter_input(INPUT_POST, 'cpf', FILTER_VALIDATE_INT);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+    $telefone = filter_input(INPUT_POST, 'telefone', FILTER_VALIDATE_INT);
 
     //atualização no BD
-    $comandoSQL = "UPDATE Tipo_Ingresso_Cliente SET id_tipo_ingresso = '$idingresso', id_cliente = '$idcliente', volume_ingresso = '$volumeingresso'
-                         WHERE id_tipo_ingresso_cliente = '$atualizarID';";
+    $comandoSQL = "UPDATE Cliente SET nome_cliente = '$nomeCliente', cpf = '$cpf', email = '$email', telefone = '$telefone'
+                         WHERE id_cliente = '$atualizarID';";
     $pdo->exec($comandoSQL);
 
-    formingresso($pdo);
-    listarIngresso($pdo);
+    form();
+    listarCliente($pdo);
 }
 ?>
