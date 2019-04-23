63b2f6a4c6040f05e54cbe92290545b5168c18c0
Raziel Willms <42423628+RazielWillms@users.noreply.github.com>
2019-04-22 21:59:58 -0300

    Update

--- menu.php
+++ menu.php
@@ -10,45 +10,32 @@
 </style>
 <body>
 <ul>
-<!--    --><?php
-//    if (session_id() != null && isset($_SESSION['nome_cliente'])) {
-//        ?>
-<!--        <li><a href="menu.php">Home</a></li>-->
-<!--        <li><a href="tipoIngresso.php">Comprar Ingressos</a></li>-->
-<!--        <li><a href="logout.php">Logout</a></li>-->
-<!--        --><?php
-//    } else {
-//        if (session_id() == null && !isset($_SESSION['nome_cliente'])) {
-//            ?>
-<!--            <li><a href="menu.php">Home</a></li>-->
-<!--            <li><a href="cadastroCliente.php">Cadastrar-se</a></li>-->
-<!--            <li><a href="login.php">login</a></li>-->
-<!--            --><?php
-//        }
-//    }
-//    ?>
-        <li><a href="menu.php">Home</a></li>
-        <li><a href="cadastroCliente.php">Cadastrar-se</a></li>
-        <li><a href="tipoIngresso.php">Comprar Ingressos</a></li>
-        <li><a href="login.php">login</a></li>
-        <li><a href="logout.php">Logout</a></li>
+    <li><a href="tipoIngresso.php">Comprar Ingressos</a></li>
+    <li><a href="cadastroCliente.php">Cadastrar-se</a></li>
+    <li><a href="login.php">Entrar</a></li>
+    <li><a href="logout.php">Sair</a></li>
 </ul>
 <imagem>
     <img src="img/sepultura.jpeg" ;><br>
 </imagem>
 
 <estilomenu>
-    SEPULTURA 23/11<br>
+    SEPULTURA<br>
 </estilomenu>
-<estilobody>
-    Estado: Rio Grande do Sul<br>
-    Cidade: Santo Ângelo<br>
-    Horário: 23:30<br>
-</estilobody>
 
+<form>
+    <input type="submit" name="action" value="Sobre o Evento"/>
+    <input type="submit" name="action" value="Pontos de Venda"/>
+    <input type="submit" name="action" value="Fotos Da Banda"/>
+    <input type="submit" name="action" value="Como Chegar"/>
+</form>
 </body>
 </html>
 
 <?php
 require_once "style.php";
+require_once "funcoesEvento.php";
+
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+choose($action);
 ?>
